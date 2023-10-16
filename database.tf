03e98727c62e0277bb191ec2adb6974fcb0ab9dc
Kacper Muda <mudakacper@gmail.com>
2023-10-16 15:41:42 +0200

    fix: Add ignore for database related checkov policies: CKV_AZURE_27 and CKV_AZURE_224

--- database.tf
+++ database.tf
@@ -9,6 +9,7 @@ resource "azurerm_mssql_server" "mlflow_db_server" {
   #checkov:skip=CKV_AZURE_23:Auditing not required
   #checkov:skip=CKV_AZURE_24:Auditing not required
   #checkov:skip=CKV_AZURE_113:Only internal azure networks are enabled, and we need public endpoint to connect from Container App
+  #checkov:skip=CKV2_AZURE_27:Ensure Azure AD authentication is enabled for Azure SQL (MSSQL)
 
   name                          = local.database_name_from_descriptor
   resource_group_name           = data.azurerm_resource_group.rg.name
@@ -30,6 +31,7 @@ resource "azurerm_mssql_firewall_rule" "mlflow_db_firewall_allow_internal" {
 }
 
 resource "azurerm_mssql_database" "mlflow_db" {
+  # checkov:skip=CKV_AZURE_224:Ensure that the Ledger feature is enabled on database that requires cryptographic proof and nonrepudiation of data integrity
   name                        = local.database_name_from_descriptor
   server_id                   = azurerm_mssql_server.mlflow_db_server.id
   max_size_gb                 = var.db_max_size_gb
